8d373ee51fc7b0e867bac225f6fdae85026eb0a0
linghe <2286340149@qq.com>
2025-06-14 21:17:44 +0800

    NavBar

--- src/app/(main)/NavBar.tsx
+++ src/app/(main)/NavBar.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function NavBar() {
-  return <header className="sticky top-0"></header>;
+  return (
+    <header className="bg-card sticky top-0 z-10 shadow-sm">
+      <div className="max-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
+        <Link href="" className="text-primary text-2xl font-bold">
+          bugbook
+        </Link>
+      </div>
+    </header>
+  );
 }
